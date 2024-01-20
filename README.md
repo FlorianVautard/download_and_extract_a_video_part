d6787c3244fb259e15070564c82275bf55c0e906
FlorianVautard <contact@florianvautard.fr>
2024-01-20 11:05:04 +0100

    Allow to download via proxy and the audio only

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # download_and_extract_a_video_part
-A Bash program to download and extract a part of a video. 
+A Bash program to download and extract a part of a video or audio. It is mainly a wrapper of [ytb-dl](https://github.com/yt-dlp/yt-dlp) and [ffmpeg](https://ffmpeg.org/download.html) in order to make it more user friendly. 
 ## Dependencies
 To run, the program needs ffmpeg and yt-dlp.
 ## Installation
@@ -7,6 +7,10 @@ To run, the program needs ffmpeg and yt-dlp.
 [https://ffmpeg.org/download.html](https://ffmpeg.org/download.html)
 ### Youtube-dl - now yt-dlp
 [https://github.com/yt-dlp/yt-dlp](https://github.com/yt-dlp/yt-dlp)
+## Configuration
+You can set a proxy_server_ip, a proxy_server_port and a tmp directory at the begining of the file.
+proxy_server_ip: <ip>
+proxy_server_port : <port>
 ## Running
 This program allows you to define the required parameter by sending the parameters directly to the program or in answering questions. You can also combine both , the parameter will be given directly and the question will not be asked.
 
@@ -15,8 +19,13 @@ An example below with the extraction of the entrance of the eyes of the tiger cl
 # Without any parameters
 > ./download_and_extract_a_video_part.sh  
 Please enter the video URL: https://www.youtube.com/watch?v=btPJPFnesV4
-At what time does the portion to keep start (in the format HH:MM:SS)? 00:00:00
-How long does the video to keep last (in the format HH:MM:SS)? 00:01:05
+Do you want to download only the audio of the video? (true/false)
+> true
+The audio will be downloaded
+At what time does the portion to keep start (in the format HH:MM:SS)? 
+> 00:00:00
+How long does the video to keep last (in the format HH:MM:SS)? 
+> 00:01:05
 What is the name of the extracted file? intro_eyes_of_the_tiger
 > ls
 intro_eyes_of_the_tiger.mp4
@@ -28,8 +37,10 @@ intro_eyes_of_the_tiger.mp4
 
 # With both, parameters and answers
 > ./download_and_extract_a_video_part.sh-u https://www.youtube.com/watch?v=btPJPFnesV4 -o intro_eyes_of_the_tiger
-At what time does the portion to keep start (in the format HH:MM:SS)? 00:00:00
-How long does the video to keep last (in the format HH:MM:SS)? 00:01:05
+At what time does the portion to keep start (in the format HH:MM:SS)? 
+> 00:00:00
+How long does the video to keep last (in the format HH:MM:SS)? 
+> 00:01:05
 > ls
 intro_eyes_of_the_tiger.mp4
 ```
--- download_and_extract_a_video_part.sh
+++ download_and_extract_a_video_part.sh
@@ -1,36 +1,17 @@
 #!/bin/bash
 
+proxy_server_ip=""
+proxy_server_port=""
+tmp_directory="/tmp"
+
 # Function to print usage information
 usage() {
-    echo "Usage: $0 [-u <video_url>] [-s <start_time>] [-t <duration>] [-o <output_file>]"
+    echo "Usage: $0 [-a Audio only] [-u <video_url>] [-s <start_time>] [-t <duration>] [-o <output_file>] [-p Use proxy for downloading] [-ps <proxy_server_ip>] [-pp <proxy_server_port>]"
     exit 1
 }
 
-# Parse command line options
-while getopts ":u:s:t:o:" opt; do
-    case $opt in
-        u)
-            video_url=$OPTARG
-            ;;
-        s)
-            start_time=$OPTARG
-            ;;
-        t)
-            duration=$OPTARG
-            ;;
-        o)
-            output=$OPTARG
-            ;;
-        \?)
-            echo "Invalid option: -$OPTARG"
-            usage
-            ;;
-        :)
-            echo "Option -$OPTARG requires an argument."
-            usage
-            ;;
-    esac
-done
+###############################################################################################
+# CHECK DEPENDENCIES
 
 # Check for the presence of yt-dlp
 if ! command -v yt-dlp &> /dev/null; then
@@ -44,51 +25,141 @@ if ! command -v ffmpeg &> /dev/null; then
     exit 1
 fi
 
+###############################################################################################
+# PARSE COMMAND LINE OPTIONS
+
+while getopts ":u:s:t:o:p:ps:pp:" opt; do
+    case $opt in
+        a)
+            audio=true
+            ;;
+        u)
+            video_url=$OPTARG
+            ;;
+        s)
+            start_time=$OPTARG
+            ;;
+        t)
+            duration=$OPTARG
+            ;;
+        o)
+            output=$OPTARG
+            ;;
+        p)
+            use_proxy=true
+            ;;
+        ps)
+            proxy_server_ip=$OPTARG
+            ;;
+        pp)
+            proxy_server_port=$OPTARG
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG"
+            usage
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument."
+            usage
+            ;;
+    esac
+done
+
+###############################################################################################
+# PROXY PART
+
+# Ask if a proxy should be used, if not provided as a parameter
+if [[ -z "$use_proxy" ]]; then
+    read -p "Do you want to use a proxy for downloading? (YES/NO): " use_proxy_input
+    case $use_proxy_input in
+        [Yy][Ee][Ss])
+            use_proxy=true
+	    echo "A proxy will be used."
+            ;;
+        *)
+            use_proxy=false
+	    echo "A proxy will not be used."
+            ;;
+    esac
+fi
+
+# Check if a proxy should be used
+if [[ "$use_proxy" == true ]]; then
+    if [[ -z "$proxy_server_ip" ]] || [[ -z "$proxy_server_port" ]]; then
+        # Ask for the proxy server details
+        read -p "Enter the proxy server address: " proxy_server_ip
+        read -p "Enter the proxy server port: " proxy_server_port
+    fi
+
+    # Establish the SSH connection for SOCKS proxy
+    ssh -D "$proxy_server_port" -q -C -N -f "$proxy_server_ip"
+    proxy_command="--proxy socks5://127.0.0.1:$proxy_server_port"
+fi
+
+###############################################################################################
+# DOWNLOAD AND EXTRACT VIDEO
+
 # Ask for the URL of the video to be kept if not provided as a parameter
 while [[ -z "$video_url" ]]; do
     read -p "Please enter the video URL: " video_url
 done
-# Get the title of the video using yt-dlp
-video_title=$(yt-dlp --get-title "$video_url")
 
-# Ask for the name of the extracted file if not provided as a parameter
-while [[ -z "$output" ]]; do
+# Ask if it is only the audio which will be downloaded
+if [[ -z "$audio" ]]; then
+	read -p "Do you want to download only the audio of the video? (true/false)" audio
+fi
+
+# If $audio is still empty, download all the video
+if [[ -z "$audio" ]]; then
+    video_format="-f bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4"
+    echo "The video with the audio will be downloaded."	
+else
+    video_format="-f bestaudio[ext=m4a]"
+    echo "The audio of the video will be downloaded."
+fi
+
+
+# Get the title of the video using yt-dlp
+video_title=$(yt-dlp $proxy_command --get-title "$video_url")
+
+# Ask for the name of the extracted file or use the video title if not provided as a parameter
+if [[ -z "$output" ]]; then
     read -p "What is the name of the video (current title is '$video_title')? " output
-done
+fi
+
+# If output is still empty, use the video title
+if [[ -z "$output" ]]; then
+    output="$video_title"
+    echo "The name of the video will be: $video_title"
+fi
 
 # Ask for the start time of the portion to keep if not provided as a parameter
 read -p "At what time does the portion to keep start (in the format HH:MM:SS)? " start_time
 
-# Check if start_time is empty, if yes, download the entire video
+# Check if start_time is empty, if yes, download the entire video, if no, ask for duration and download a part of the video
 if [[ -z "$start_time" ]]; then
-    # Set the backup path to /tmp
-    tmp_directory="/tmp"
-
+    echo "The entire video will be downloaded."
     # Download the video with yt-dlp and save it in /tmp
-    yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' "$video_url" --output "$output.mp4"
+    yt-dlp $proxy_command "$video_format" "$video_url" --output "$output.mp4"
 
     echo "The entire video has been successfully downloaded. The output file is $output.mp4."
-    exit 0
 else
-	# If start_time is provided, ask for the duration
-	while [[ -z "$duration" ]]; do
-	    read -p "How long does the video to keep last (in the format HH:MM:SS)? " duration
-	done
+    # If start_time is provided, ask for the duration
+    while [[ -z "$duration" ]]; do
+        read -p "How long does the video to keep last (in the format HH:MM:SS)? " duration
+    done
+    
+    # Download the video with yt-dlp and save it in /tmp
+    yt-dlp $proxy_command "$video_format" "$video_url" --output "$tmp_directory/video.mp4"
+
+    # Use ffmpeg to cut the video
+    ffmpeg -ss "$start_time" -t "$duration" -i "$tmp_directory/video.mp4" "$tmp_directory/$output.mp4"
+
+    # Move the cut video to the current directory
+    mv "$tmp_directory/$output.mp4" .
+    
+    # Remove the temporary video file
+    rm "$tmp_directory/video.mp4"
+    
+    echo "The video has been successfully cut. The output file is '$output.mp4'."
 fi
-
-# Set the backup path to /tmp
-tmp_directory="/tmp"
-
-# Download the video with yt-dlp and save it in /tmp
-yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' "$video_url" --output "$tmp_directory/video.mp4"
-
-# Use ffmpeg to cut the video
-ffmpeg -ss "$start_time" -t "$duration" -i "$tmp_directory/video.mp4" "$tmp_directory/$output.mp4"
-
-# Move the cut video to the current directory
-mv "$tmp_directory/$output.mp4" .
-
-# Remove the temporary video file
-rm "$tmp_directory/video.mp4"
-
-echo "The video has been successfully cut. The output file is '$output.mp4'."
